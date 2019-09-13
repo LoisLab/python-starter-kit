39faf1e67ff48e27cd7a3a5ef7e13bd4cea136ed
Michael Megliola <michael.megliola@gmail.com>
2019-09-13 08:28:05 -0400

    Create dictionaries.py

---
+++ week1/dictionaries.py
@@ -0,0 +1,17 @@
+'''
+takeaway: a dictionary stores labeled attributes
+'''
+
+'''
+Here are the attributes of a sword
+'''
+sword = {'weight': 6, 'roll': 12, 'damage': 6}
+
+'''
+Here is my D&D character: Mikhail the Slow
+'''
+me = {}
+me['name'] = 'Mikhail the Slow'
+me['intelligence'] = 3
+me['deterimination'] = 62
+me['weapon'] = sword
