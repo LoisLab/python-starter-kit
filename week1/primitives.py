1f6febbba7f703129c60d108404a1393b708ca4d
Michael Megliola <michael.megliola@gmail.com>
2019-09-13 08:24:52 -0400

    Create primitives.py

---
+++ week1/primitives.py
@@ -0,0 +1,42 @@
+'''
+takeaway: Python supports lots of types of data
+'''
+
+# integer
+a=1
+b=1+2
+
+# floating point number
+c=2.345
+d=1+2.3
+
+# complex number (j = sqrt(-1))
+e=4-8j
+f=4+8j
+g=e*f
+
+# string
+h='you look to the left and see a cave'
+i=' ...but I would not suggest entering...'
+j=h+i
+k=h*3
+
+# boolean
+l=True
+m=7>5
+n=True or False
+
+# conversions
+p=float('1.234')
+q=complex(3,3)
+r=str(complex(3,3))
+s=str(1.2345)
+
+# operators
+t=7%5
+u=7/5
+v=7//5
+w=not True
+x=not not True
+y=float('inf')
+z=y*2
